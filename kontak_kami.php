cc6f254cf361b63fef2432feb8b086251e769ced
esteban <43982498@continental.edu.pe>
2018-11-15 17:33:08 -0500

    Initial commit

---
+++ kontak_kami.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+<title></title>
+    <meta charset="utf-8">
+    <title>Sobre Nosotros - Charitos Store</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+     <style type="text/css">
+          p{
+            text-indent:50px;
+          }
+      </style>
+    <!-- Le styles  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet"/>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet"/>
+	<link href="assets/css/docs.css" rel="stylesheet"/>
+	 
+    <link href="style.css" rel="stylesheet"/>
+	<link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+	
+	<!-- Less styles  
+	<link rel="stylesheet/less" type="text/css" href="less/bootsshop.less">
+	<script src="less.js" type="text/javascript"></script>
+	 -->
+	
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+
+</head>
+<body>
+<div class="span9" id="mainCol">
+		<ul class="breadcrumb">
+			<li><a href="?open=''">Inicio</a><span class="divider">/</span></li>
+			<li class="active">Contactenos</li>
+			<li>Corporativo</li>
+		</ul>
+		<h3>Contactenos</h3>
+		<hr class="soft">
+			<h3><font color="red">Charitos Store</font> <font color="green">SIT</font></h3><br/>
+	
+        <font face:'comic sans';>
+<strong>Contactenos</strong><br>
+<p>
+Como  Tienda Online están cerca de nuestros clientes también abriría oportunidades. Número de teléfono móvil 920162520. También puede contactar con nosotros a través del correo electrónico: maritahriveros@gmail.com
+Puede preguntar acerca de nuestros productos que van desde el precio hasta que el depósito de bienes disponibles.
+Máximo servicio a los consumidores es el objetivo de nuestros servicios de comercio.</p> 
+ <div class="span4">
+	<iframe  width="790" height="250" frameborder="0" scrolling="no" marginheight="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.597141761504!2d-75.20947995156791!3d-12.071215591409088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x910e964e010b3885%3A0x99f994c36997fda7!2sPiura+%26+Ancash%2C+Huancayo+12000!5e0!3m2!1ses!2spe!4v1542310383801" width="1170" height="220" frameborder="0" style="border:0"></iframe><br /><small><a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.597141761504!2d-75.20947995156791!3d-12.071215591409088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x910e964e010b3885%3A0x99f994c36997fda7!2sPiura+%26+Ancash%2C+Huancayo+12000!5e0!3m2!1ses!2spe!4v1542310383801" style="color:#0000FF;text-align:left">Ver Mapa Más Grande</a></small>
+	</div>
+</div>
+</div>
+</body>
+</html>
